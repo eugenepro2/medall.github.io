138300d86f7b9fa3d6d6772611e2c5e24f7b7e5c
eugenepro2 <eugenepro@icloud.com>
2018-06-15 20:03:42 +0300

    upload

--- src/js/import/slider.js
+++ src/js/import/slider.js
@@ -1,8 +1,26 @@
 import 'owl.carousel';
 
-$('.owl-carousel').owlCarousel({
-  margin:15,
-  loop:true,
-  autoWidth:true,
-  items:4
+
+let certificate = $('.certificate-slider');
+certificate.owlCarousel({
+  margin: 15,
+  loop: true,
+  autoWidth: true,
+  items: 4
+});
+
+
+let tabsReview = $('.review-slider');
+tabsReview.owlCarousel({
+  margin: 15,
+  loop: true,
+  items: 1,
+});
+
+$('.tabs__review__next').click(function() {
+  tabsReview.trigger('next.owl.carousel');
+});
+// Go to the previous item
+$('.tabs__review__prev').click(function() {
+  tabsReview.trigger('prev.owl.carousel');
 });
