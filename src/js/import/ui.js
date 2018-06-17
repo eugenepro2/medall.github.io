92dbb496902584d276081a273fa7f9f08e5e407a
eugenepro2 <eugenepro@icloud.com>
2018-06-17 13:25:15 +0300

    upload

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -6,3 +6,24 @@ $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
     .addClass('active').siblings().removeClass('active')
     .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
+
+//Kind
+function Kind() {
+  $('.kind__big, .kind__tabs, .kind__small').each(function(i) {
+    if ($(window).width() < 1024) {
+      return false;
+    }
+    let w;
+    if (i < 5) {
+      w = $(this).width();
+    } else{
+      w = $(this).prev().width();
+    }
+    $(this).height(w);
+
+  });
+}
+$(window).on('resize', function() {
+  Kind();
+});
+Kind();
