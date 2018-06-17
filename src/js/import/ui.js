da5e4c4990fdcfa9138cf8d4bf4e9f5ae7dbf7f3
eugenepro2 <eugenepro@icloud.com>
2018-06-17 20:22:09 +0300

    upload

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -8,26 +8,26 @@ $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
     .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 });
 
-//Kind
-function Kind() {
-  $('.kind__big, .kind__tabs, .kind__small').each(function(i) {
-    if ($(window).width() < 1024) {
-      return false;
-    }
-    let w;
-    if (i < 5) {
-      w = $(this).width();
-    } else{
-      w = $(this).prev().width();
-    }
-    $(this).height(w);
+// //Kind
+// function Kind() {
+//   $('.kind__big, .kind__tabs, .kind__small').each(function(i) {
+//     if ($(window).width() < 1024) {
+//       return false;
+//     }
+//     let w;
+//     if (i < 5) {
+//       w = $(this).width();
+//     } else{
+//       w = $(this).prev().width();
+//     }
+//     $(this).height(w);
 
-  });
-}
-$(window).on('resize', function() {
-  Kind();
-});
-Kind();
+//   });
+// }
+// $(window).on('resize', function() {
+//   Kind();
+// });
+// Kind();
 
 
 //SlideOut
